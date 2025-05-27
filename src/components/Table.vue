5f29994b01751bba3f90dc5a551f1f3d9a001d26
Justin Navarro <info@justinnavarro.net>
2025-05-26 21:07:13 -0400

    created provide/inject to the table component

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,43 @@
 <script setup lang="ts">
+import { computed, provide, ref } from 'vue';
+import { type TableColumn, TableContext, type TableContextType } from '../types/table.ts';
 import TablePagination from './TablePagination.vue';
 import TableHeaders from './TableHeaders.vue';
 import TableBody from './TableBody.vue';
+import type { Optional } from '../types/generics.ts';
+
+const headers = ref<Record<string, TableColumn>>({});
+
+const getHeaders = computed<TableColumn[]>(() => Object.values(headers.value));
+
+const getColSpan = (col?: number) => {
+    const widthClasses = {
+        '1': 'w-1/12',
+        '2': 'w-2/12',
+        '3': 'w-3/12',
+        '4': 'w-4/12',
+        '5': 'w-5/12',
+        '6': 'w-6/12',
+        '7': 'w-7/12',
+        '8': 'w-8/12',
+        '9': 'w-9/12',
+        '10': 'w-10/12',
+        '11': 'w-11/12',
+        '12': 'w-full',
+    };
+    const idx = `${col || 1}` as keyof typeof widthClasses;
+    return widthClasses[idx] || 'w-full';
+};
+
+const setHeaders = (header: Record<string, Optional<TableColumn, 'width'>>) => {
+    const [id, data] = Object.entries(header)[0];
+    headers.value = { ...headers.value, [id]: { ...data, width: getColSpan(data.colspan) } };
+};
+
+provide<TableContextType>(TableContext, {
+    getHeaders,
+    setHeaders,
+});
 </script>
 
 <template>
@@ -10,7 +46,9 @@ import TableBody from './TableBody.vue';
     >
         <TableHeaders />
 
-        <TableBody />
+        <TableBody>
+            <slot />
+        </TableBody>
 
         <TablePagination />
     </div>
--- src/components/TableHeaders.vue
+++ src/components/TableHeaders.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject } from 'vue';
+import { TableContext, type TableContextType } from '../types/table.ts';
+
+const tableContext = inject<TableContextType>(TableContext);
+</script>
 
 <template>
     <div
         class="border-lm-stroke text-lm-text-hover dark:border-dm-stroke dark:text-dm-text-hover flex flex-row items-center justify-between space-x-4 border-b px-4 py-4"
     >
-        Column Headers
+        <template v-for="(column, index) in tableContext?.getHeaders?.value" :key="index">
+            <div :class="column.width">{{ column.label }}</div>
+        </template>
     </div>
 </template>
