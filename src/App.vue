36040297034fe46f85295082bf001868b1b92fae
Justin Navarro <info@justinnavarro.net>
2025-05-26 21:07:32 -0400

    created column basic component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import Table from './components/Table.vue';
+import ColumnBasic from './components/ColumnBasic.vue';
 </script>
 
 <template>
     <h1 class="bg-mint-500 mb-4">Table</h1>
 
     <div class="p-4">
-        <Table></Table>
+        <Table>
+            <ColumnBasic header="Column 1">Column One</ColumnBasic>
+            <ColumnBasic header="Column 2">Column Two</ColumnBasic>
+        </Table>
     </div>
 </template>
---
+++ src/components/ColumnBasic.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { inject, onMounted, useId } from 'vue';
+import { TableContext, type TableContextType } from '../types/table.ts';
+
+interface Props {
+    header: string;
+}
+
+const props = defineProps<Props>();
+
+const id = useId();
+
+const tableContext = inject<TableContextType>(TableContext);
+
+onMounted(() => {
+    console.log(tableContext);
+    if (tableContext) {
+        tableContext.setHeaders({
+            [id]: {
+                colspan: 1,
+                label: props.header,
+            },
+        });
+    }
+});
+</script>
+
+<template>
+    <p><slot /></p>
+</template>
