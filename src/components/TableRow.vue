7b87eb0a906b3b1833becf1c03ecc69c33a8a08c
Justin Navarro <info@justinnavarro.net>
2025-05-31 19:41:58 -0400

    created the TableRow component

---
+++ src/components/TableRow.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { provide } from 'vue';
+import { TableRowContext, type TableRowContextType } from '../types/table.ts';
+
+interface Props {
+    data: { row: any };
+}
+
+const props = defineProps<Props>();
+
+const parseValue = (value: string) => {
+    return value.split('.').reduce((result, current) => {
+        return result[current] ?? '';
+    }, props.data?.row);
+};
+
+provide<TableRowContextType>(TableRowContext, { data: props.data?.row, parseValue });
+</script>
+
+<template>
+    <slot></slot>
+</template>
--- src/types/table.ts
+++ src/types/table.ts
@@ -2,6 +2,7 @@ import type { ComputedRef } from 'vue';
 import type { Optional } from './generics.ts';
 
 export const TableContext = Symbol('TableContext');
+export const TableRowContext = Symbol('TableRowContext');
 
 export interface TableColumn {
     colspan: number;
@@ -11,6 +12,12 @@ export interface TableColumn {
 
 export interface TableContextType {
     data: any[];
+    getColSpan: (cols: number) => string;
     getHeaders: ComputedRef<readonly TableColumn[]>;
     setHeaders: (header: Record<string, Optional<TableColumn, 'width'>>) => void;
+}
+
+export interface TableRowContextType {
+    data: any;
+    parseValue: (value: string) => string;
 }
