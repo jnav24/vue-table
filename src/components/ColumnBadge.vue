bad905ffceb6e3cdb76379b87b6051d00dd0ae54
Justin Navarro <info@justinnavarro.net>
2025-06-07 10:10:13 -0400

    started column badge

---
+++ src/components/ColumnBadge.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Typography from './Typography.vue';
+</script>
+
+<template>
+    <div class="inline-block rounded-lg border p-2">
+        <Typography variant="caption"></Typography>
+    </div>
+</template>
