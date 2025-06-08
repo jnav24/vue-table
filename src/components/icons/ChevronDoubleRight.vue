d9e5a28ba96c2f986c5e8825ff193065d0603db7
Justin Navarro <info@justinnavarro.net>
2025-06-08 16:23:42 -0400

    created icons components

---
+++ src/components/icons/ChevronDoubleRight.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{ classes: string }>();
+</script>
+
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        :class="classes"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+        />
+    </svg>
+</template>
